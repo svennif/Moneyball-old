269f3c0b3240e8c41d8b8f6cd878992e243ed590
Sven Ingar Frantzen <sven.ingar@gmail.com>
2023-12-12 22:12:19 +0100

    Changes

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
+
+
 import pandas as pd
 from bs4 import BeautifulSoup
 from IPython.display import display, HTML
